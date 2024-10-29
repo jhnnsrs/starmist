5bbaa5bccbbb8f8b29bca8549fb7ead20e9cce35
Johannes Roos <jhnnsrs@gmail.com>
2024-10-29 09:47:40 +0100

    initial commit with limited functionality

---
+++ README.md
@@ -0,0 +1,16 @@
+# Starmist
+
+Starmist is Stardist enabled for the Arkitekt platform. You can check out the original Stardist repository [here](https://github.com/mpicbg-csbd/stardist).
+
+## Installation
+
+To install Starmist, you need to have the Arkitekt-Next platform installed. 
+
+## Comparison to Segmentor
+
+Starmist currently only supports 2D images, while Segmentor supports 2D and 3D images. Additionalliy Starmist does not support training yet, As of now, Starmist is only a prediction tool.
+
+## Roadmap
+
+- [ ] Add support for 3D images
+- [ ] Add support for training (utilizing a new model backend)
---
+++ app.py
@@ -0,0 +1,68 @@
+from stardist.models import StarDist3D
+from csbdeep.utils import Path, normalize
+import sys
+import numpy as np
+import xarray as xr
+from mikro_next.api.schema import (
+   from_array_like, 
+   Image,
+   PartialDerivedViewInput,
+   PartialPixelViewInput,
+)
+import numpy as np
+from pydantic import Field
+from arkitekt_next.tqdm import tqdm as atdqm
+from arkitekt_next import easy
+from stardist import (
+    fill_label_holes,
+    random_label_cmap,
+    calculate_extents,
+    gputools_available,
+)
+from stardist import Rays_GoldenSpiral
+from stardist.models import StarDist2D
+from stardist.matching import matching, matching_dataset
+from stardist.models import Config3D, StarDist3D, StarDistData3D
+from tqdm import tqdm
+import shutil
+import uuid
+from arkitekt_next import register
+from enum import Enum
+from typing import Optional
+from concurrent.futures import ProcessPoolExecutor
+
+
+class PreTrainedModels(str, Enum):
+    STARDIST_ORGANOID_3D = "stardist3"
+    STARDIST_STYLED = "stardist_styled"
+
+
+
+@register(collections=["segmentation", "prediction", "nuclei"])
+def predict_flou2(image: Image) -> Image:
+    """Segment Flou2
+
+    Segments Cells using the stardist flou2 pretrained model
+
+    Args:
+        image (Image): The Input Image.
+    Returns:
+        Image: An Image with the Segmented Cells.
+
+    """
+    model = StarDist2D.from_pretrained("2D_versatile_fluo")
+    x = image.data.sel(c=0, t=0, z=0).transpose(*"xy").data.compute()
+    x = normalize(x)
+
+    labels, details = model.predict_instances(x)
+
+    array = xr.DataArray(labels, dims=list("xy"))
+
+    nana = from_array_like(
+        array,
+        name="Segmented " + image.name,
+        derived_views=[PartialDerivedViewInput(originImage=image)],
+        pixel_views=[PartialPixelViewInput()],
+    )
+    return nana
+
